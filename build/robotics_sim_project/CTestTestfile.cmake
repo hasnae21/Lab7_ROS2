e376d93b9d599f0f9f5c57411a4927c64f13cd88
Hasnae AHOUZI <ahouzi.hasnae.solicode@gmail.com>
2026-01-01 01:18:12 +0100

    Stop tracking build artifacts

--- build/robotics_sim_project/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hasnae/ros2_ws7/src/robotics_sim_project
-# Build directory: /home/hasnae/ros2_ws7/build/robotics_sim_project
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
